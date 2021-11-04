93f5d2aa1826f4f88ecc9c0d8c6fda58967e5e7b
OrdasWingson <ordaswingson@gmail.com>
2021-11-04 16:19:42 +0300

    -переработан алгоритм Signed-off-by: OrdasWingson <ordaswingson@gmail.com>

--- BlockDiskConsole.cpp
+++ BlockDiskConsole.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <windows.h>
-#include <fileapi.h>
+
+#define SIZE_ARRAY 512 
 
 using namespace std;
 
@@ -25,8 +26,17 @@ void umount(HANDLE hDisk)
     cout << "UNMOUNT" << endl;
 }
 
-void write(BYTE* buf, bool what)
+DWORD64 getVolumeSize(BYTE* buf)
 {
+    DWORD64 sz = *(DWORD64*)&buf[40];
+    int16_t sectors_numb = *(int16_t*)&buf[11];
+    sz *= sectors_numb;
+    return sz;
+}
+
+DWORD64 firs_sector_handl(BYTE* buf, bool what)
+{
+    DWORD64 sz;
     string fsName = "";
     for (int i = 3; i < 7; i++)
     {
@@ -35,75 +45,85 @@ void write(BYTE* buf, bool what)
     
     if (what) //1 - lock 0 - unlock
     {
-        for (int i = 0; i < 4096; i++) buf[i] = buf[i]++;
         
-       /*if (fsName == "NTFS")
+        if (fsName == "NTFS")
         {
-            for (int i =0; i < sizeof(buf); i++) buf[i] = buf[i]++;
+            int r = sizeof(buf);
+            sz = getVolumeSize(buf);
+            for (int i =0; i < SIZE_ARRAY; i++) buf[i] = buf[i]++;
         }
-        else cout << "Error of file system inditifaction\n";*/
+        else cout << "Error of file system inditifaction\n";
         
     }
     else 
     {
-        for (int i = 0; i < 4096; i++)  buf[i] = buf[i]--;
-        /*if (fsName == "OUGT")
+        if (fsName == "OUGT")
         {
-            for (int i = 0; i < sizeof(buf); i++)  buf[i] = buf[i]--;
+            for (int i = 0; i < SIZE_ARRAY; i++)  buf[i] = buf[i]--;
+            sz = getVolumeSize(buf);
         }
-        else cout << "Error of signature\n";*/
+        else cout << "Error of signature\n";
     }
-   
+    return sz;
 }
 
-void block(HANDLE hVolume, DWORD64 sz, bool what)
+
+
+void block(HANDLE hVolume, wstring valueName, bool what)
 {
-    BYTE buf[512];
-    DWORD dwRead;
-    int count;
-    
-    LARGE_INTEGER li = {(DWORD)sz,sz>>32};
-    //count = SetFilePointer(hVolume, li.LowPart + 3584, &li.HighPart, FILE_BEGIN);
+    BYTE buf[SIZE_ARRAY];
+    DWORD dwRead;    
     SetFilePointer(hVolume, 0, NULL, FILE_BEGIN);
+    if (!::ReadFile(hVolume, buf, sizeof(buf), &dwRead, NULL) || dwRead != sizeof(buf)) throw win_error("Can't read a volume.");
+   
+    int r = sizeof(buf);
+    int g = 45;
+    SetFilePointer(hVolume, 0, NULL, FILE_BEGIN);
+    DWORD64 sz = firs_sector_handl(buf, what);
+    LARGE_INTEGER li = { (DWORD)sz,sz >> 32 };
+    if (!::WriteFile(hVolume, buf, sizeof(buf), &dwRead, NULL))  throw win_error("Can't write a volume.");
     umount(hVolume);
+    SetFilePointer(hVolume, li.LowPart, &li.HighPart, FILE_BEGIN);
     if (!::ReadFile(hVolume, buf, sizeof(buf), &dwRead, NULL) || dwRead != sizeof(buf)) throw win_error("Can't read a volume.");
-    //count = SetFilePointer(hVolume, li.LowPart+3584, &li.HighPart, FILE_BEGIN);
-    SetFilePointer(hVolume, 0, NULL, FILE_BEGIN);
-    //write(buf, what);
-    //if (!::WriteFile(hVolume, buf, sizeof(buf), &dwRead, NULL))  throw win_error("Can't write a volume.");
-    
-    SetFilePointer(hVolume, li.LowPart+512, &li.HighPart, FILE_BEGIN);
-    if (!::ReadFile(hVolume, buf, sizeof(buf), &dwRead, NULL) || dwRead != sizeof(buf)) throw win_error("Can't read a volume.");
-    int Error = GetLastError();
-    SetFilePointer(hVolume, li.LowPart, &li.HighPart, FILE_END);
+    SetFilePointer(hVolume, li.LowPart, &li.HighPart, FILE_BEGIN);
+    firs_sector_handl(buf, what);
+    if (!::WriteFile(hVolume, buf, sizeof(buf), &dwRead, NULL))  throw win_error("Can't write a volume.");
 }
-
+void show_device()
+{
+    char drivers[256];
+    GetLogicalDriveStrings((DWORD)sizeof(drivers), (LPWSTR)drivers);
+    int k = GetLastError();
+    cout << "Discs on computer: ";
+    for (int i = 0; i < sizeof(drivers); i++)
+    {
+        
+        if (i % 8 == 0)
+        {
+            if (drivers[i] == '\0') return;
+            cout << drivers[i] << ":\\ ";
+        }
+    }
+}
     
 
 int main()
 {
     wstring valueName;
     bool what;
-    cout << "Text name of value (C, D or other letter): ";
+    show_device();
+    cout << "\nChoose the disk (C, D or other letter): ";
     wcin >> valueName;
     cout << "bloc or unbloc (1 or 0)?: ";
     cin >> what;
     wstring valueNameFile = L"\\\\.\\" + valueName + L":";
     valueName = valueName + L":\\";
-    DWORD lpSectorsPerCluster = 0;
-    DWORD lpBytesPerSector = 0;
-    DWORD lpNumberOfFreeClusters = 0;
-    DWORD lpTotalNumberOfClusters = 0;
-    bool r = GetDiskFreeSpace(valueName.c_str(), &lpSectorsPerCluster, &lpBytesPerSector, &lpNumberOfFreeClusters, &lpTotalNumberOfClusters);
-    DWORD64 sz = lpTotalNumberOfClusters;
-    sz *= lpBytesPerSector * lpSectorsPerCluster;
-    DWORD d = GetLastError();
     HANDLE hDisk = CreateFile(valueNameFile.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     
     DWORD dwBytesRead;
-    
+
     if (hDisk == INVALID_HANDLE_VALUE) throw win_error("INVALID_HANDLE_VALUE");//\\\\.\\PhysicalDrive0 \\\\.\\D:
-    bool w = DeviceIoControl(hDisk, FSCTL_ALLOW_EXTENDED_DASD_IO, NULL, 0, NULL, 0, &dwBytesRead, NULL);
-    block(hDisk, sz,what);
+    DeviceIoControl(hDisk, FSCTL_ALLOW_EXTENDED_DASD_IO, NULL, 0, NULL, 0, &dwBytesRead, NULL);
+    block(hDisk, valueName, what);
     ::CloseHandle(hDisk);
 }
